9c1d3605ca7e801782d7a9697ad9a1f3e0da876b
Victhor gabriel freire de oliveira <144156974+VirtuProbs@users.noreply.github.com>
2024-12-06 16:38:27 -0300

    Add files via upload

---
+++ TYPESCRIPT/src/test.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+;
+function saudacao(nome) {
+    return "Olá" + nome;
+}
+;
+console.log(multiplicar(2, 4));
+console.log(saudacao("Antonio"));
